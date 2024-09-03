c2f8cdfd4a229104f69936aeec188468e53c44af
rabiarz <rabiarezaie21@gmail.com>
2024-09-03 19:14:35 +0900

    auth/login-signup

---
+++ README.md
@@ -0,0 +1,2 @@
+# port => http://localhost:3000 
+# to run the server use => npm run devStart
---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')  /* import libaraies */
+const app = express()
+const bcrypt = require('bcrypt')
+
+app.use(express.json()) //allow the server to accept data as jason file 
+
+const users = [] // an empoty defult array to stor data
+
+app.get ('/users', (req, res) => {  // give the response as a json file
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  try{
+    const hashPassword = await bcrypt.hash(req.body.password, 10) // wait for the password and save it as a hash  
+    const user = {name: req.body.name, password: hashPassword}  // creat a user with name and hash pasword
+    users.push(user)  // add the user
+    res.status(201).send() // sends a response (created) if it was successful
+  } catch {
+    res.status(500).send() // sends a response (server error) if creation was failed
+  }
+})
+
+app.post('/users/login', async (req, res) => { //authenticate
+  const user = users.find(user => user.name === req.body.name) // Searches for the user 
+  if (user == null) {
+    return res.status(400).send("Can not find user") // if user not found gives error
+  } 
+  try{
+    if (await bcrypt.compare(req.body.password, user.password)){ //compare the req password with the usre password
+      res.send('Succes') // sends a succes response if the password was correct.
+    } else {
+      res.send('Not allowed') //sends a not allowed response if the password was incorrect.
+    }
+    }catch{
+   res.status(500).send() //server error
+  }
+})
+
+app.listen(3000) // port
