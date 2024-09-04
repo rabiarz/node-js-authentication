935aae625a3d97f23ee6366dcd03118a59d0057c
rabiarz <rabiarezaie21@gmail.com>
2024-09-04 17:51:04 +0900

    creat-post

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express')  /* import libaraies */
 const app = express()
 const bcrypt = require('bcrypt')
+const fs = require('fs');
+const { title } = require('process');
 
 app.use(express.json()) //allow the server to accept data as jason file 
 
 const users = [] // an empoty defult array to stor data
+const posts = []
 
 app.get ('/users', (req, res) => {  // give the response as a json file
   res.json(users)
@@ -19,6 +22,10 @@ app.post('/users', async (req, res) => {
   } catch {
     res.status(500).send() // sends a response (server error) if creation was failed
   }
+})
+
+app.get ('/users/login', (req, res) => {  // give the response as a json file
+  res.json(users)
 })
 
 app.post('/users/login', async (req, res) => { //authenticate
@@ -36,5 +43,24 @@ app.post('/users/login', async (req, res) => { //authenticate
    res.status(500).send() //server error
   }
 })
+
+app.get ('/posts', (req, res) => {  // give the response as a json file
+  res.json(posts)
+})
+
+app.post('/posts', (req, res) => {
+  
+  if (!req.body.title || !req.body.content) {
+    return res.status(400).send("title and content is required!")
+  }
+  const post = { 
+    title: req.body.title, 
+    content: req.body.content,
+    createdAt: new Date()
+  }
+  posts.push(post)
+  res.status(201).send(post)
+})
+
 
 app.listen(3000) // port
